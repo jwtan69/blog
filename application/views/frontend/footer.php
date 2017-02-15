9e1df3eb72de6cdd3031a2d0423b293ac5d410a0
jwtan <jwtan69@gmail.com>
2017-02-15 15:55:56 +0800

    back up

--- application/views/frontend/footer.php
+++ application/views/frontend/footer.php
@@ -47,7 +47,7 @@
 	
 	<div class="copyright wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
 		<div class="container">
-			<p>© 2016 Style Blog. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
+			<p>© 2017 BaNaNa866. All rights reserved | Design by <a href="http://w3layouts.com/">W3layouts</a></p>
 		</div>
 	</div>
 </body>
