a98b5c3565f9a57af5c5944a403b0c7e1634939c
jwtan <jwtan69@gmail.com>
2017-05-03 20:14:33 +0800

    a

--- application/views/frontend/details.php
+++ application/views/frontend/details.php
@@ -11,7 +11,25 @@
 			<div class="agileinfo">
 
 		  <h2 class="w3"><?=$articleData['article_variable']?></h2>
-			
+			<div class="single">
+			   <img src="<?=$articleData['main_img']?>" class="img-responsive" alt="">
+			    <div class="b-bottom"> 
+			      
+			      <!-- Go to www.addthis.com/dashboard to customize your tools --> 
+			      <div style="margin:20px 0px;" class="addthis_inline_share_toolbox"></div>
+
+			      <div style="margin:30px 0px;">
+			      <?=$articleData['content_cn']?>
+				  </div> 
+
+				  <div style="margin:30px 0px;">
+			      <span style="font-size:14px;color:red;">如转载涉及版权等问题请原作者与我们联系(668ananab@gmail.com)我们将在第一时间处理，谢谢！</span>
+				  </div> 
+
+
+				  <p><?=date('d-m-Y',strtotime($articleData['created_date']))?> <a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span><?=$articleData['view_count']?></a></p>
+				</div>
+			 </div>
 			  
 				
 				<div class="clearfix"></div>
@@ -35,7 +53,16 @@
 						</div>
 					<h4>熱門文章 </h4>
 						<?php foreach($popularArticle as $k => $v){?>
-			            
+			            <div class="blog-grids wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
+			              <div class="blog-grid-left">
+			                
+			              </div>
+			              <div class="blog-grid-right">
+			                
+			                
+			              </div>
+			              <div class="clearfix"> </div>
+			            </div>
 			            <?php }?>
 
 						
