f0a58d690b539e8ac9bbc12d8d4a8aacb8d76d69
jwtan <jwtan69@gmail.com>
2017-05-03 20:06:36 +0800

    a

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -65,7 +65,116 @@
 
   -->
 
-  
+  <!-- technology-left -->
+  <div class="technology">
+  <div class="container">
+    <div class="col-md-9 technology-left">
+    <div class="tech-no">
+
+      <!-- technology-top -->
+      <?php if(isset($artResult1)){?>
+       <div class="tc-ch wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
+        
+          <div class="tch-img">
+            <a href="<?=base_url($init['langu'].'/details/'.$artResult1[0]['article_id'].'/'.$artResult1[0]['article_variable'])?>"><img src="<?=$artResult1[0]['main_img']?>" class="img-responsive" alt=""></a>
+          </div>
+          
+          <h3>
+          <a href="<?=base_url($init['langu'].'/details/'.$artResult1[0]['article_id'].'/'.$artResult1[0]['article_variable'])?>"><?=$artResult1[0]['article_variable']?></a>
+          </h3>
+          <h6><?=date('d-m-Y',strtotime($artResult1[0]['created_date']))?></h6>
+            <p><?=mb_substr($artResult1[0]['page_desc'],0,100)?>...</p>
+            <!--
+            <div class="bht1">
+              <a href="singlepage.html">Continue Reading</a>
+            </div>
+            <div class="soci">
+              <ul>
+                <li class="hvr-rectangle-out"><a class="fb" href="#"></a></li>
+                <li class="hvr-rectangle-out"><a class="twit" href="#"></a></li>
+                <li class="hvr-rectangle-out"><a class="goog" href="#"></a></li>
+                <li class="hvr-rectangle-out"><a class="pin" href="#"></a></li>
+                <li class="hvr-rectangle-out"><a class="drib" href="#"></a></li>
+              </ul>
+            </div>
+            -->
+            <div class="clearfix"></div>
+      </div>
+      <div class="clearfix"></div>
+      <?php }?>
+      <!-- technology-top -->
+
+
+      <!-- technology-top -->
+      <?php if(isset($artResult2)){?>
+      <div class="w3ls">
+        <?php foreach($artResult2 as $k => $v){?>
+        <div class="col-md-6 w3ls-left wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
+           <div class="tc-ch">
+            <div class="tch-img">
+              <a href="<?=base_url($init['langu'].'/details/'.$v['article_id'].'/'.$v['article_variable'])?>">
+              <img src="<?=$v['main_img']?>" class="img-responsive" alt=""></a>
+            </div>
+          
+            <h3><a href="<?=base_url($init['langu'].'/details/'.$v['article_id'].'/'.$v['article_variable'])?>"><?=$v['article_variable']?></a></h3>
+            <h6><?=date('d-m-Y',strtotime($v['created_date']))?></h6>
+              <p><?=mb_substr($v['page_desc'],0,100)?>...</p>
+              <!--
+              <div class="bht1">
+                <a href="singlepage.html">Read More</a>
+              </div>
+              <div class="soci">
+                <ul>
+                  <li class="hvr-rectangle-out"><a class="fb" href="#"></a></li>
+                  <li class="hvr-rectangle-out"><a class="pin" href="#"></a></li>
+                </ul>
+              </div>
+              -->
+              <div class="clearfix"></div>
+          </div>
+        </div>
+        <?php }?>
+        <div class="clearfix"></div>
+      </div>
+      <?php }?>
+      <!-- technology-top -->
+
+      <?php if(isset($artResult3)){?>
+      <?php foreach($artResult3 as $k => $v){?>
+      <div class="wthree">
+         <div class="col-md-6 wthree-left wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
+          <div class="tch-img">
+              <a href="<?=base_url($init['langu'].'/details/'.$v['article_id'].'/'.$v['article_variable'])?>"><img src="<?=$v['main_img']?>" class="img-responsive" alt=""></a>
+            </div>
+         </div>
+         <div class="col-md-6 wthree-right wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
+            <h3><a href="<?=base_url($init['langu'].'/details/'.$v['article_id'].'/'.$v['article_variable'])?>"><?=$v['article_variable']?></a></h3>
+            <h6><?=date('d-m-Y',strtotime($v['created_date']))?></h6>
+              <p><?=mb_substr($v['page_desc'],0,100)?>...</p>
+              
+              <!--
+              <div class="bht1">
+                <a href="singlepage.html">Read More</a>
+              </div>
+              <div class="soci">
+                <ul>
+                  
+                  <li class="hvr-rectangle-out"><a class="twit" href="#"></a></li>
+                  <li class="hvr-rectangle-out"><a class="pin" href="#"></a></li>
+                </ul>
+              </div>
+              -->
+              <div class="clearfix"></div>
+          
+         </div>
+         <div class="clearfix"></div>
+      </div>
+      <?php }?>
+      <?php }?>
+      
+      
+      </div>
+    </div>
     <!-- technology-right -->
     <div class="col-md-3 technology-right">
         
@@ -74,7 +183,7 @@
               
           <div class="tech-btm">
           
-          <!--<?=$fbSharePageCode?>-->
+          <?=$fbSharePageCode?>
           
           <div class="search-1 wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
               <form action="" method="" onsubmit="toSearch();">
@@ -85,7 +194,7 @@
 
           <h4>熱門文章 </h4>
 
-            
+            <?php foreach($popularArticle as $k => $v){?>
             <div class="blog-grids wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
               <div class="blog-grid-left">
                 <a href="<?=base_url($init['langu'].'/details/'.$v['article_id'])?>"><img src="<?=$v['main_img']?>" class="img-responsive" alt=""></a>
@@ -96,7 +205,7 @@
               </div>
               <div class="clearfix"> </div>
             </div>
-            
+            <?php }?>
 
            
            
