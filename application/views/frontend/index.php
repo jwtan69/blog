f5215f5b9b52e21fcbff9753e7dd295cb08f82ea
jwtan <jwtan69@gmail.com>
2017-05-03 20:02:00 +0800

    a

--- application/views/frontend/index.php
+++ application/views/frontend/index.php
@@ -66,52 +66,7 @@
   -->
 
   
-    <!-- technology-right -->
-    <div class="col-md-3 technology-right">
-        
-        
-        <div class="blo-top1">
-              
-          <div class="tech-btm">
-          
-          <!--<?=$fbSharePageCode?>-->
-          
-          <div class="search-1 wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
-              <form action="" method="" onsubmit="toSearch();">
-                <input type="search" name="Search" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}" required="">
-                <input type="submit" value=" ">
-              </form>
-            </div>
-
-          <h4>熱門文章 </h4>
-
-            <?php foreach($popularArticle as $k => $v){?>
-            <div class="blog-grids wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
-              <div class="blog-grid-left">
-                <a href="<?=base_url($init['langu'].'/details/'.$v['article_id'])?>"><img src="<?=$v['main_img']?>" class="img-responsive" alt=""></a>
-              </div>
-              <div class="blog-grid-right">
-                
-                <h5><a href="<?=base_url($init['langu'].'/details/'.$v['article_id'])?>"><?=$v['article_variable']?></a> </h5>
-              </div>
-              <div class="clearfix"> </div>
-            </div>
-            <?php }?>
-
-           
-           
-          <div class="insta wow fadeInDown"  data-wow-duration=".8s" data-wow-delay=".2s">
-          <h4>Ad</h4>
-            
-          </div>
-          </div>
-          
-          
-          
-        </div>
-        
-      
-    </div>
+  
     <div class="clearfix"></div>
     <!-- technology-right -->
   </div>
