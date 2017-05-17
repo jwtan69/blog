11feceeb51c5d7a6897a5c070b6a58a66024d090
jwtan <jwtan69@gmail.com>
2017-05-17 23:57:02 +0800

    bb

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -197,7 +197,7 @@ class Welcome extends CI_Controller {
 
 		header('Content-Type: application/json; charset=utf-8');
         header("Access-Control-Allow-Origin: *");
-        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+        //header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 
 		if(isset($_SERVER["CONTENT_TYPE"]) && strpos($_SERVER["CONTENT_TYPE"], "application/json") !== false) {
             $_POST = array_merge($_POST, (array) json_decode(trim(file_get_contents('php://input')), true));
@@ -213,19 +213,22 @@ class Welcome extends CI_Controller {
 
 	 		$postdata2 = $this->input->post("token", true);
 
+	 		//$data = $_REQUEST;
 
+			$json = array(
+				'token'=>json_encode($postdata2),
+			);
+
+			$this->DeviceToken_model->insert($json);
+
+			$respone = array('status'=>'ok');
+
+ 		}else{
+
+ 			$respone = array('status'=>'fail');
  		}
 
-
-		//$data = $_REQUEST;
-
-		$json = array(
-			'token'=>json_encode($postdata2),
-		);
-
-		$this->DeviceToken_model->insert($json);
-
-		$respone = array('status'=>'ok');
+		
 		echo json_encode($respone);
 
 		exit;
