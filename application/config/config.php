1200701380568408abd390fb367b16d91eff8f95
jwtan <jwtan69@gmail.com>
2017-08-29 15:58:31 +0800

    save

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 if(isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == "blog.local"){
 $config['base_url'] = 'http://blog.local/';
 }else{
-$config['base_url'] = 'http://www.banana866.com/';
+$config['base_url'] = 'https://www.banana866.com/';
 }
 
 
@@ -377,7 +377,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
